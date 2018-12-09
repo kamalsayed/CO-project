d321e722acc65d9cb089330241f9b5f020138c92
kamalsayed <44102251+kamalsayed@users.noreply.github.com>
2018-12-09 20:43:13 +0200

    Update main.c

--- main.c
+++ main.c
@@ -42,11 +42,9 @@ int main()
 int BubbleSort_both(int *array , int type, int size){
 
     int i ,j,tmp ;
-    if (type == 0){
+    for(i=0;i<size-1;i++){ //i=5
 
-    for(i=0;i<size-1;i++){
-
-        for(j=0;j<size-1-i;j++){
+        for(j=0;j<size-1-i;j++){//0--->14-5 : 9
 
             if(array[j]>array[j+1]){
                 tmp = array[j];
@@ -56,25 +54,18 @@ int BubbleSort_both(int *array , int type, int size){
         }//nested
     }//outer
 
-    }//end of ascending
-    else if(type == 1){
-        for(i=0;i<size-1;i++){
-
-        for(j=0;j<size-1-i;j++){
-
-            if(array[j]<array[j+1]){
-                tmp = array[j];
-                array[j]=array[j+1];
-                array[j+1]=tmp;
-            }
-        }//nested
-    }//outer
-
-    }//end of descending
-
-
+   if (type == 0){
+    return;
+   }
+   else if(type==1){
+    for(i=0,j = size-1;i<(size-1)/2;i++, j--){
+        tmp = array[j]; // from last indexes in  the array
+        array[j]=array[i];
+        array[i]=tmp;
+    }
+   }
+    //end of descending
 }
-
 
 int search_(int *array,int key ,int size){
 int i;
